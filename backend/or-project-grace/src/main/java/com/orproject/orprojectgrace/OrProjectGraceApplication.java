a925c223669feff8f181c837c5cee89497cb6fa1
websiteyarn <104047557+websiteyarn@users.noreply.github.com>
2024-03-19 21:09:24 +0800

    or

---
+++ backend/or-project-grace/src/main/java/com/orproject/orprojectgrace/OrProjectGraceApplication.java
@@ -0,0 +1,13 @@
+package com.orproject.orprojectgrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrProjectGraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrProjectGraceApplication.class, args);
+	}
+
+}
